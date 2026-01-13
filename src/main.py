914f76bebea2bfcaadf9355eec41f53389bc3ca0
Irakli Skhirtladze <irskhirtladze@gmail.com>
2026-01-13 08:14:13 +0000

    Changes

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from generator.gen import generate_imgs, dataset_to_hf
 
 if __name__ == "__main__":
     while True:
-        user_input = input("How many images for font would you like to generate?: ")
+        user_input = input("How many images per font would you like to generate?: ")
         if not user_input.isdigit() or int(user_input) <= 0:
             print("\nPlease enter a positive integer.")
             continue
